13045ef95553be37651a47de99b948ab831a4c23
sneha1505 <87608797+sneha1505@users.noreply.github.com>
2023-08-03 06:39:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+
+const sunButton = document.getElementById("sun-button");
+const sunPopup = document.getElementById("sun-popup");
+
+sunButton.addEventListener("click", () => {
+  sunPopup.style.display = "flex";
+});
+
+sunPopup.addEventListener("click", () => {
+  sunPopup.style.display = "none";
+});
+
+const windButton = document.getElementById("wind-button");
+const windPopup = document.getElementById("wind-popup");
+
+windButton.addEventListener("click", () => {
+  windPopup.style.display = "flex";
+});
+
+windPopup.addEventListener("click", () => {
+  windPopup.style.display = "none";
+});
+
+
+
+const cloudButton = document.getElementById("cloud-button");
+const cloudPopup = document.getElementById("cloud-popup");
+
+cloudButton.addEventListener("click", () => {
+  cloudPopup.style.display = "flex";
+});
+
+cloudPopup.addEventListener("click", () => {
+  cloudPopup.style.display = "none";
+});
+
+
+const thunderButton = document.getElementById("thunder-button");
+const thunderPopup = document.getElementById("thunder-popup");
+
+thunderButton.addEventListener("click", () => {
+  thunderPopup.style.display = "flex";
+});
+
+thunderPopup.addEventListener("click", () => {
+  thunderPopup.style.display = "none";
+});
+
+
+const rainButton = document.getElementById("rain-button");
+const rainPopup = document.getElementById("rain-popup");
+
+rainButton.addEventListener("click", () => {
+  rainPopup.style.display = "flex";
+});
+
+rainPopup.addEventListener("click", () => {
+  rainPopup.style.display = "none";
+});
+
+
+
+
+
+const rainbowButton = document.getElementById("rainbow-button");
+const rainbowPopup = document.getElementById("rainbow-popup");
+
+rainbowButton.addEventListener("click", () => {
+  rainbowPopup.style.display = "flex";
+});
+
+rainbowPopup.addEventListener("click", () => {
+  rainbowPopup.style.display = "none";
+});
